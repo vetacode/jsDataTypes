bb316a5b23f7c4f971658d0b7e748f0964defd5d
vetacode <vetacode@gmail.com>
2025-11-06 20:50:19 +0700

    Task 4 done

--- numbers.js
+++ numbers.js
@@ -157,3 +157,24 @@ while (i < 11) {
   i += 0.2;
   i > 9.8 && i < 10.2 ? console.log(i) : null;
 }
+
+/**
+ * TASK 4
+ * A random number from min to max
+importance: 2
+The built-in function Math.random() creates a random value from 0 to 1 (not including 1).
+
+Write the function random(min, max) to generate a random floating-point number from min to max (not including max).
+
+Examples of its work:
+
+alert( random(1, 5) ); // 1.2345623452
+alert( random(1, 5) ); // 3.7894332423
+alert( random(1, 5) ); // 4.3435234525
+ */
+
+function random(min, max) {
+  return min + Math.random() * (max - min);
+}
+
+console.log(random(1, 10));
