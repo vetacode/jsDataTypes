38e96e795c8d1eeb208c2c9938e7200f42974807
vetacode <vetacode@gmail.com>
2025-11-06 21:54:54 +0700

    Task 5 Done

--- numbers.js
+++ numbers.js
@@ -178,3 +178,24 @@ function random(min, max) {
 }
 
 console.log(random(1, 10));
+
+/**TASK 5
+ * A random integer from min to max
+importance: 2
+Create a function randomInteger(min, max) that generates a random integer number from min to max including both min and max as possible values.
+
+Any number from the interval min..max must appear with the same probability.
+
+Examples of its work:
+
+alert( randomInteger(1, 5) ); // 1
+alert( randomInteger(1, 5) ); // 3
+alert( randomInteger(1, 5) ); // 5
+ */
+
+function randomInteger(min, max) {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+
+  return Math.round(rand);
+}
+console.log(randomInteger(2, 5));
