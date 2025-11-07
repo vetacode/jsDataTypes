49dd2606ca069a5e16d60d88d392cd8740db43a1
vetacode <vetacode@gmail.com>
2025-11-07 15:46:34 +0700

    task 4 done

--- strings.js
+++ strings.js
@@ -184,3 +184,27 @@ function checkSpam3(str) {
 console.log(checkSpam3('buy ViAgRA now'));
 console.log(checkSpam3('free xxxxx'));
 console.log(checkSpam3('innocent rabbit'));
+
+/** TASK 3
+ * Truncate the text
+importance: 5
+Create a function truncate(str, maxlength) that checks the length of the str and, if it exceeds maxlength – replaces the end of str with the ellipsis character "…", to make its length equal to maxlength.
+
+The result of the function should be the truncated (if needed) string.
+
+For instance:
+
+truncate("What I'd like to tell on this topic is:", 20) == "What I'd like to te…"
+
+truncate("Hi everyone!", 20) == "Hi everyone!"
+ */
+
+function truncate(str, maxlength) {
+  return str.length > maxlength ? str.slice(0, maxlength - 1) + '...' : str;
+}
+
+let a = truncate("What I'd like to tell on this topic is:", 20);
+console.log(a);
+
+let b = truncate('Hi everyone!', 20);
+console.log(b);
