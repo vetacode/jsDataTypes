abf318c1b21cd7c272aab689380142ab5f0cb357
vetacode <vetacode@gmail.com>
2025-11-08 11:53:13 +0700

    pausing on task 5

--- arrays.js
+++ arrays.js
@@ -209,3 +209,37 @@ arrayy.push(function () {
 
 let x = arrayy[2]();
 console.log(x);
+
+/** TASK 5
+ * A maximal subarray
+importance: 2
+The input is an array of numbers, e.g. arr = [1, -2, 3, 4, -9, 6].
+
+The task is: find the contiguous subarray of arr with the maximal sum of items.
+
+Write the function getMaxSubSum(arr) that will return that sum.
+
+For instance:
+
+getMaxSubSum([-1, 2, 3, -9]) == 5 (the sum of highlighted items)
+getMaxSubSum([2, -1, 2, 3, -9]) == 6
+getMaxSubSum([-1, 2, 3, -9, 11]) == 11
+getMaxSubSum([-2, -1, 1, 2]) == 3
+getMaxSubSum([100, -9, 2, -3, 5]) == 100
+getMaxSubSum([1, 2, 3]) == 6 (take all)
+If all items are negative, it means that we take none (the subarray is empty), so the sum is zero:
+
+getMaxSubSum([-1, -2, -3]) = 0
+Please try to think of a fast solution: O(n2) or even O(n) if you can.
+ */
+
+let arrei = [1, -2, 3, 4, -9, 6];
+let currentSum = 0;
+let maxSum = 0;
+
+function getMaxSubSum(arrei) {
+  for (let currentSum = 0; )
+
+}
+
+console.log(arrei);
