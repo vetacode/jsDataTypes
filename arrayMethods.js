f488f2f6d82520d56c85f93971d2137a6c66c58e
vetacode <vetacode@gmail.com>
2025-11-09 20:52:14 +0700

    adding splice

--- arrayMethods.js
+++ arrayMethods.js
@@ -4,3 +4,9 @@ let console = ['ps', 'xbox', 'nintendo'];
 
 console.splice(0, 1, 'sega');
 console.log(console);
+
+console.splice(3, 0, 'ps', 'super nintendo');
+console.log(console);
+
+console.splice(-2, 2);
+console.log(console);
