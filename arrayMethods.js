039bafefc336796d756e12db2172b49cd1bde622
vetacode <vetacode@gmail.com>
2025-11-09 21:34:06 +0700

    adding concat

--- arrayMethods.js
+++ arrayMethods.js
@@ -25,3 +25,32 @@ console.log(fruits === newFruits); //false
 console.log(newFruits);
 
 console.log(fruits.slice(-2)); //copy from -2 untill the end of arr
+
+//concat: creates new array + values from other array + additional items
+//syntax: arr.concat(arg1, arg2...)
+let arr = [1, 2];
+
+// create an array from: arr and [3,4]
+console.log(arr.concat([3, 4])); // 1,2,3,4
+
+// create an array from: arr and [3,4] and [5,6]
+console.log(arr.concat([3, 4], [5, 6])); // 1,2,3,4,5,6
+
+// create an array from: arr and [3,4], then add values 5 and 6
+console.log(arr.concat([3, 4], 5, 6)); // 1,2,3,4,5,6
+
+let arrayLike = {
+  0: 'something',
+  length: 1,
+};
+
+console.log(arr.concat(arrayLike)); // 1,2,[object Object]
+
+let arrayLike2 = {
+  0: 'something',
+  1: 'else',
+  [Symbol.isConcatSpreadable]: true,
+  length: 2,
+};
+
+console.log(arr.concat(arrayLike2)); // 1,2,something,else
