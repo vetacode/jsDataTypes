cf1ad72294b3ba9b2db6b313a65b82953d61f165
vetacode <vetacode@gmail.com>
2025-11-10 17:37:44 +0700

    task 2 completed

--- arrayMethods.js
+++ arrayMethods.js
@@ -336,3 +336,29 @@ function camelize(str) {
   return splitter.join('');
 }
 console.log(camelize(strings));
+
+/**TASK 2
+ * Filter range
+importance: 4
+Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+
+The function should not modify the array. It should return the new array.
+
+For instance:
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (matching values)
+
+alert( arr ); // 5,3,8,1 (not modified) */
+
+let numbres = [5, 3, 8, 1];
+
+function filterRange(arr, a, b) {
+  return arr.filter((item) => a <= item && item <= b);
+}
+let filtered = filterRange(numbres, 1, 4);
+console.log(filtered);
+console.log(numbres);
