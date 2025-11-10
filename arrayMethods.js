4d8962bb3078ea769bcdc6f96338f9e37ed91b5e
vetacode <vetacode@gmail.com>
2025-11-10 11:10:18 +0700

    adding reduce method

--- arrayMethods.js
+++ arrayMethods.js
@@ -204,6 +204,16 @@ console.log(namaku);
 //join
 console.log(arr3.join(', '));
 
+//REDUCE and REDUCE RIGHT
+//syntax: let value = arr.reduce(function(accumulator, item, index, array) {
+// ... }, [initial]);
+//simple usable syntax: let value = arr.reduce((sum, current) => sum + current, 0);
+
+let angka = [2, 3, 1, 5, 6, 2, 1];
+
+let jumlah = angka.reduce((sum, current) => sum + current, 0);
+console.log(jumlah);
+
 /*
 | Method       | Returns        | Purpose                             |
 | ------------ | -------------- | ----------------------------------- |
