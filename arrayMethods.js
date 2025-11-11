3a6b1e3879f3c6d4a78f75b4df5fad03940da4f9
vetacode <vetacode@gmail.com>
2025-11-11 18:12:59 +0700

    final task 13 has been solved

--- arrayMethods.js
+++ arrayMethods.js
@@ -732,3 +732,53 @@ let str = [
 ];
 
 console.log(unique(str)); // Hare, Krishna, :-O
+
+/**TASK 13
+ * Create keyed object from array
+importance: 4
+Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
+For example:
+
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+/
+Such function is really handy when working with server data.
+
+In this task we assume that id is unique. There may be no two array items with the same id.
+
+Please use array .reduce method in the solution.
+ */
+
+let users5 = [
+  { id: 'john', name: 'John Smith', age: 20 },
+  { id: 'ann', name: 'Ann Smith', age: 24 },
+  { id: 'pete', name: 'Pete Peterson', age: 31 },
+];
+
+function groupById(arr) {
+  return arr.reduce((a, b) => {
+    a[b.id] = b;
+    return a;
+  }, {});
+}
+
+let usersById = groupById(users5);
+
+console.log(usersById);
