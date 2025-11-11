b8d06310614a44b53e09fd5d9fdb5a2102c2654c
vetacode <vetacode@gmail.com>
2025-11-11 16:34:43 +0700

    task 8 done

--- arrayMethods.js
+++ arrayMethods.js
@@ -524,3 +524,47 @@ let users3 = [john, pete, mary];
 let names2 = users3.map((item) => item.name);
 
 console.log(names2);
+
+/**TASK 8
+ * Map to objects
+importance: 5
+You have an array of user objects, each one has name, surname and id.
+
+Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+For instance:
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = /* ... your code ... */
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+/*
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
+So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
+ */
+
+let john2 = { name: 'John', surname: 'Smith', id: 1 };
+let pete2 = { name: 'Pete', surname: 'Hunt', id: 2 };
+let mary2 = { name: 'Mary', surname: 'Key', id: 3 };
+
+let users4 = [john2, pete2, mary2];
+
+let usersMapped = users4.map((user) => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: `${user.id}`,
+}));
+
+console.log(usersMapped[0].id);
+console.log(usersMapped[0].fullName);
