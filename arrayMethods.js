c844687b301f56426fa7619d5f4121d86b46659a
vetacode <vetacode@gmail.com>
2025-11-11 17:30:00 +0700

    adding Knuth Suffle methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -606,3 +606,55 @@ console.log(userSort);
 console.log(arrai[0].name);
 console.log(arrai[1].name);
 console.log(arrai[2].name);
+
+/**TASK 10
+ * Shuffle an array
+importance: 3
+Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+
+Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+let arr = [1, 2, 3];
+
+shuffle(arr);
+// arr = [3, 2, 1]
+
+shuffle(arr);
+// arr = [2, 1, 3]
+
+shuffle(arr);
+// arr = [3, 1, 2]
+// ...
+All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] or [3,1,2] etc, with equal probability of each case.
+*/
+
+let arr1 = [1, 2, 3];
+
+function shuffle(arr) {
+  return arr.sort(() => Math.random() - 0.5);
+}
+
+console.log(shuffle(arr1));
+console.log(shuffle(arr1));
+console.log(shuffle(arr1));
+
+//use Fisher-Yates Shuffles Method
+
+let arr10 = [1, 2, 3];
+
+function shuffle2(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+console.log(shuffle2(arr10));
+console.log(shuffle2(arr10));
+console.log(shuffle2(arr10));
