ee66e8ee420c8468d25fcc86fd47b19b9151a4b9
vetacode <vetacode@gmail.com>
2025-11-11 16:16:04 +0700

    task 7 accomplished

--- arrayMethods.js
+++ arrayMethods.js
@@ -281,15 +281,15 @@ console.log(soldiers2);
 */
 
 /*
-| Method       | Returns        | Purpose                             |
-| ------------ | -------------- | ----------------------------------- |
-| `.forEach()` | `undefined`    | Just loops (for side effects)       |
-| `.map()`     | New array      | Transforms each element             |
-| `.filter()`  | New array      | Keeps elements that match condition |
-| `.find()`    | Single element | Returns the first that matches      |
-| for.. of     | string         | loops                               |
-| .split       | Arrray from strings                                  |
-| .join        | Strings from array
+| Method       | Returns        | FROM            | Purpose                             |
+| ------------ | --------------  ---------------- | ----------------------------------- |
+| `.forEach()` | `undefined`    |                 | Just loops (for side effects)       |
+| `.map()`     | New array      | Array           | Transforms each element             |
+| `.filter()`  | New array      |                 | Keeps elements that match condition |
+| `.find()`    | Single element |                 | Returns the first that matches      |
+| for.. of     | string         |                 | loops                               |
+| .split       | Arrray         | Strings         |
+| .join        | Strings        | Array           |
 
 */
 
@@ -497,3 +497,30 @@ console.log(calc.calculate('3 + 7')); // 10
 
 calc.addMethods('*', (a, b) => a * b);
 console.log(calc.calculate('4 * 5')); // 20
+
+/**TASK 7
+ * Map to names
+importance: 5
+You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+
+For instance:
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names =  /... your code/
+
+alert( names ); // John, Pete, Mary */
+
+let john = { name: 'John', age: 25 };
+let pete = { name: 'Pete', age: 30 };
+let mary = { name: 'Mary', age: 28 };
+
+let users3 = [john, pete, mary];
+
+let names2 = users3.map((item) => item.name);
+
+console.log(names2);
