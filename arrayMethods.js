c4d803553bd57f376d7462ee629ddb302325f2f5
vetacode <vetacode@gmail.com>
2025-11-11 18:04:36 +0700

    task 12 done

--- arrayMethods.js
+++ arrayMethods.js
@@ -689,3 +689,46 @@ function getAverageAge(arr) {
 }
 
 console.log(getAverageAge(arr11));
+
+/** TASK 12
+ * Filter unique array members
+importance: 4
+Let arr be an array.
+
+Create a function unique(arr) that should return an array with unique items of arr.
+
+For instance:
+
+function unique(arr) {
+  / your code /
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(strings) ); // Hare, Krishna, :-O
+ */
+
+function unique(arr) {
+  let result = [];
+
+  for (let string of arr) {
+    if (!result.includes(string)) result.push(string);
+  }
+  return result;
+}
+
+let str = [
+  'Hare',
+  'Krishna',
+  'Hare',
+  'Krishna',
+  'Krishna',
+  'Krishna',
+  'Hare',
+  'Hare',
+  ':-O',
+];
+
+console.log(unique(str)); // Hare, Krishna, :-O
