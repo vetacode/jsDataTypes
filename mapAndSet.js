49cdb6aae2c447387d70d64de302222b213f7e92
vetacode <vetacode@gmail.com>
2025-11-12 13:07:18 +0700

    adding map and set methods

---
+++ mapAndSet.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Map
+Map is a collection of keyed data items, just like an Object. But the main difference is that Map allows keys of any type.
+
+Methods and properties are:
+new Map() – creates the map.
+map.set(key, value) – stores the value by the key.
+map.get(key) – returns the value by the key, undefined if key doesn’t exist in map.
+map.has(key) – returns true if the key exists, false otherwise.
+map.delete(key) – removes the element (the key/value pair) by the key.
+map.clear() – removes everything from the map.
+map.size – returns the current element count.
+ */
+
+let map = new Map();
+
+map.set('1', 'str1'); // a string key
+map.set(1, 'num1'); // a numeric key
+map.set(true, 'bool1'); // a boolean key
+
+// The regular Object would convert keys to string
+// Map keeps the type, so these two are different:
+console.log(map.get(1)); // 'num1'
+console.log(map.get('1')); // 'str1'
+
+console.log(map.size); // 3
