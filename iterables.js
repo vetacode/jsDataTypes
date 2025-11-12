abc984f194a465e72d652e1f3c743bc18d7136c4
vetacode <vetacode@gmail.com>
2025-11-12 10:06:48 +0700

    separating symbol iterator with next()

--- iterables.js
+++ iterables.js
@@ -29,3 +29,25 @@ for (let num of range) {
 }
 
 console.log(iter);
+
+let range2 = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() {
+    this.current = this.from;
+    return this;
+  },
+
+  next() {
+    if (this.current <= this.to) {
+      return { done: false, value: this.current++ };
+    } else {
+      return { done: true };
+    }
+  },
+};
+
+for (let num of range2) {
+  console.log(num); // 1, then 2, 3, 4, 5
+}
