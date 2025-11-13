e67638bc5f9bfd1e0847bfd019434ad0d5d914ea
vetacode <vetacode@gmail.com>
2025-11-13 20:03:52 +0700

    adding SET method

--- mapAndSet.js
+++ mapAndSet.js
@@ -138,3 +138,15 @@ console.log(halloween.entries());
 let halloweenObj = Object.fromEntries(halloween.entries()); //we can omit the .entries()
 console.log(halloweenObj);
 console.log(halloweenObj.draculi);
+
+/** SET
+ * A Set is a special type collection – “set of values” (without keys), where each value may occur only once.
+
+Its main methods are:
+new Set([iterable]) – creates the set, and if an iterable object is provided (usually an array), copies values from it into the set.
+set.add(value) – adds a value, returns the set itself.
+set.delete(value) – removes the value, returns true if value existed at the moment of the call, otherwise false.
+set.has(value) – returns true if the value exists in the set, otherwise false.
+set.clear() – removes everything from the set.
+set.size – is the elements count.
+ */
