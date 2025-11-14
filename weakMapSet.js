062717771060da905950a53ecae545bb702ea36d
vetacode <vetacode@gmail.com>
2025-11-14 08:37:51 +0700

    adding obj into array preserved from garbaged collected

--- weakMapSet.js
+++ weakMapSet.js
@@ -5,3 +5,20 @@ console.log(john); //{}
 
 john = null; //removed the obj john from memory
 console.log(john); //null
+
+//But if we put the obj reference into array, it wont we garbaged collected
+let matt = { name: 'Matt' };
+let array = [matt];
+console.log(array);
+matt = null; //remove matt ref
+console.log(matt);
+console.log(array);
+console.log(array[0]); //but the obj inside the array is still there and we can get it (matt object) as an array[0]
+
+//Sama juga kalo kita simpen di Map
+let amy = { name: 'Amy' };
+
+let map = new Map();
+map.set(amy, '...');
+
+console.log(map);
