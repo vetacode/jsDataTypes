543af3c23d2d8dc0cdca32526c2101773fb22046
vetacode <vetacode@gmail.com>
2025-11-14 23:48:21 +0700

    rask1 added

--- objectKeysValEnt.js
+++ objectKeysValEnt.js
@@ -64,3 +64,23 @@ let doublePrices = Object.fromEntries(
 );
 
 console.log(doublePrices.meat); // 8
+
+/**TASK 1
+ * Sum the properties
+importance: 5
+There is a salaries object with arbitrary number of salaries.
+
+Write the function sumSalaries(salaries) that returns the sum of all salaries using Object.values and the for..of loop.
+
+If salaries is empty, then the result must be 0.
+
+For instance:
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+alert( sumSalaries(salaries) ); // 650
+ */
