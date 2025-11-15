4503741ab27dcdcad956100e82951b9435467f53
vetacode <vetacode@gmail.com>
2025-11-15 09:40:07 +0700

    adding destructuring assign

---
+++ destructuring.js
@@ -0,0 +1,14 @@
+/**
+ * Array destructuring
+Here’s an example of how an array is destructured into variables
+ */
+
+let arr = ['John', 'Smith'];
+
+// destructuring assignment
+// sets firstName = arr[0]
+// and surname = arr[1]
+let [firstName, surname] = arr;
+
+console.log(firstName); // John
+console.log(surname); // Smith
