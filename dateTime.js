0bbfbaaf52ae9cc48e8fae752347ed4ee053267f
vetacode <vetacode@gmail.com>
2025-11-16 21:44:51 +0700

    adding Date and Time

---
+++ dateTime.js
@@ -0,0 +1,15 @@
+//Date Creation
+let now = new Date();
+console.log(now);
+
+//new Date(milliseconds)
+let jan01_1970 = new Date(0);
+console.log(jan01_1970);
+let jan02_1970 = new Date(24 * 3600 * 1000);
+console.log(jan02_1970);
+
+// 31 Dec 1969
+let Dec31_1969 = new Date(-24 * 3600 * 1000);
+console.log(Dec31_1969);
+
+//new Date(datestring)
