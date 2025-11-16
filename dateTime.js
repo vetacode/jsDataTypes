00e4b03b6988254e85a286b5a45cd385b1f476d3
vetacode <vetacode@gmail.com>
2025-11-16 21:58:48 +0700

    adding access date components

--- dateTime.js
+++ dateTime.js
@@ -13,3 +13,27 @@ let Dec31_1969 = new Date(-24 * 3600 * 1000);
 console.log(Dec31_1969);
 
 //new Date(datestring)
+let date = new Date('2017-01-26');
+console.log(date);
+// The time is not set, so it's assumed to be midnight GMT and
+// is adjusted according to the timezone the code is run in
+// So the result could be
+// Thu Jan 26 2017 11:00:00 GMT+1100 (Australian Eastern Daylight Time)
+// or
+// Wed Jan 25 2017 16:00:00 GMT-0800 (Pacific Standard Time)
+
+//new Date(year, month, date, hours, minutes, seconds, ms)
+new Date(2011, 0, 1, 0, 0, 0, 0); // 1 Jan 2011, 00:00:00
+new Date(2011, 0, 1); // the same, hours etc are 0 by default
+let date2 = new Date(2011, 0, 1, 2, 3, 4, 567);
+console.log(date2); // 1.01.2011, 02:03:04.567
+
+//ACCESS DATE COMPONENTS
+getFullYear(); //4 digits
+getMonth(); //from 0 to 11
+getDate(); //from 1 to 31
+getHours();
+getMinutes();
+getSeconds();
+getMilliseconds();
+getDay(); //from 0 (sunday) to 6 (saturday)
