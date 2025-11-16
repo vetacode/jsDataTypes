c9b31389ad8f9c5aaeb81b73fa3a062f6de45042
vetacode <vetacode@gmail.com>
2025-11-16 20:25:11 +0700

    moving the task into destructuring

--- destructuring.js
+++ destructuring.js
@@ -246,3 +246,67 @@ function mauMakan({
 
 mauMakan(pilihan);
 mauMakan({});
+
+/**TASK 1
+ * Destructuring assignment
+importance: 5
+We have an object:
+
+let user = {
+  name: "John",
+  years: 30
+};
+Write the destructuring assignment that reads:
+
+name property into the variable name.
+years property into the variable age.
+isAdmin property into the variable isAdmin (false, if no such property)
+Here’s an example of the values after your assignment:
+
+let user = { name: "John", years: 30 };
+
+// your code to the left side:
+// ... = user
+
+alert( name ); // John
+alert( age ); // 30
+alert( isAdmin ); // false
+ */
+
+let user3 = {
+  name: 'John',
+  years: 30,
+};
+
+let { name, years: age, isAdmin = false } = user3;
+
+console.log(name);
+console.log(age);
+console.log(isAdmin);
+
+/**TASK 2
+ * The maximal salary
+importance: 5
+There is a salaries object:
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+Create the function topSalary(salaries) that returns the name of the top-paid person.
+
+If salaries is empty, it should return null.
+If there are multiple top-paid persons, return any of them.
+P.S. Use Object.entries and destructuring to iterate over key/value pairs.
+ */
+
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
+};
+
+function topSalary(salaries) {
+  return ``;
+}
