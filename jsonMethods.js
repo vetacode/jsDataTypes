0a837ba46530a8504f003c609d4b452ba2acdbf8
vetacode <vetacode@gmail.com>
2025-11-17 21:31:00 +0700

    task 2 done

--- jsonMethods.js
+++ jsonMethods.js
@@ -200,3 +200,58 @@ let user3 = {
 
 let userObj = JSON.parse(JSON.stringify(user3));
 console.log(userObj);
+
+/**
+ * Exclude backreferences
+importance: 5
+In simple cases of circular references, we can exclude an offending property from serialization by its name.
+
+But sometimes we can’t just use the name, as it may be used both in circular references and normal properties. So we can check the property by its value.
+
+Write replacer function to stringify everything, but remove properties that reference meetup:
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  occupiedBy: [{name: "John"}, {name: "Alice"}],
+  place: room
+};
+
+// circular references
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+  /your code /
+}));
+
+/* result should be:
+{
+  "title":"Conference",
+  "occupiedBy":[{"name":"John"},{"name":"Alice"}],
+  "place":{"number":23}
+}
+/
+ */
+let room3 = {
+  number: 23,
+};
+
+let meetup3 = {
+  title: 'Conference',
+  occupiedBy: [{ name: 'John' }, { name: 'Alice' }],
+  place: room3,
+};
+
+// circular references
+room3.occupiedBy = meetup3;
+meetup3.self = meetup3;
+
+console.log(
+  JSON.stringify(meetup3, function replacer(key, value) {
+    return key != '' && value == meetup3 ? undefined : value;
+  })
+);
