839327eb2e57cb1abf028885570df79ee6126d47
vetacode <vetacode@gmail.com>
2025-11-17 15:15:41 +0700

    task 8 completed

--- dateTime.js
+++ dateTime.js
@@ -366,5 +366,56 @@ function getSecondsToTomorrow() {
   let totalSecondsDay = 24 * 3600;
   return totalSecondsDay - totalSecondsToNow;
 }
-
 console.log(getSecondsToTomorrow());
+
+/**TASK 8
+ * Format the relative date
+importance: 4
+Write a function formatDate(date) that should format date as follows:
+
+If since date passed less than 1 second, then "right now".
+Otherwise, if since date passed less than 1 minute, then "n sec. ago".
+Otherwise, if less than an hour, then "m min. ago".
+Otherwise, the full date in the format "DD.MM.YY HH:mm". That is: "day.month.year hours:minutes", all in 2-digit format, e.g. 31.12.16 10:00.
+For instance:
+
+alert( formatDate(new Date(new Date - 1)) ); // "right now"
+
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 sec. ago"
+
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 min. ago"
+
+// yesterday's date like 31.12.16 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) );
+ */
+
+function formatDate(date) {
+  let diff = new Date() - date;
+
+  let sec = Math.round(diff / 1000);
+  let min = Math.round(diff / 60000);
+
+  if (diff < 1000) {
+    return 'right now';
+  } else if (sec < 60) {
+    return `${sec} sec. ago`;
+  } else if (min < 60) {
+    return `${min} min. ago`;
+  }
+
+  let d = date;
+  d = [
+    '0 ' + d.getDate(),
+    '0' + d.getMonth(),
+    '' + d.getFullYear(),
+    '0' + d.getHours(),
+    '0' + d.getMinutes(),
+  ].map((item) => item.slice(-2));
+
+  // return `${d[0]}.${d[1]}.${d[2]} ${d[3]}:${d[4]}`;
+  return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+}
+console.log(formatDate(new Date(new Date() - 1))); // "right now"
+console.log(formatDate(new Date(new Date() - 30 * 1000))); // "30 sec. ago"
+console.log(formatDate(new Date(new Date() - 5 * 60 * 1000))); // "5 min. ago"
+console.log(formatDate(new Date(new Date() - 86400 * 1000)));
