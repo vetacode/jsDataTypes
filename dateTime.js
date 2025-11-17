bf2aec75c79f72377cd73750090836907a457c86
vetacode <vetacode@gmail.com>
2025-11-17 11:28:56 +0700

    adding benchmarking

--- dateTime.js
+++ dateTime.js
@@ -123,3 +123,26 @@ for (let i = 0; i < 100000; i++) {
 let ending = Date.now();
 console.log(ending.toLocaleString());
 console.log(`${ending - current} ms`); //results is faster around 1-3ms
+
+//BENCHMARKING
+//Using .getTime() is much faster coz no type conversion
+
+function diffSubtract(date1, date2) {
+  return date2 - date1;
+}
+
+function diffGetTime(date1, date2) {
+  return date2.getTime() - date1.getTime();
+}
+
+function bench(f) {
+  let date1 = new Date(0);
+  let date2 = new Date();
+
+  let start = Date.now();
+  for (let i = 0; i < 100000; i++) f(date1, date2);
+  return Date.now() - start;
+}
+
+console.log('Time of diffSubtract: ' + bench(diffSubtract) + 'ms');
+console.log('Time of diffGetTime: ' + bench(diffGetTime) + 'ms');
