5b4f48986b4caa41d2a9e40f45c23a1469bb3d4f
vetacode <vetacode@gmail.com>
2025-11-17 13:47:39 +0700

    task 6 done

--- dateTime.js
+++ dateTime.js
@@ -318,3 +318,24 @@ function getLastDayOfMonth(year, month) {
 console.log(getLastDayOfMonth(2012, 0)); // 31 (Jan)
 console.log(getLastDayOfMonth(2012, 1)); // 29 (leap year, Feb)
 console.log(getLastDayOfMonth(2013, 1)); // 28 (Feb)
+
+/**TASK 6
+ * How many seconds have passed today?
+importance: 5
+Write a function getSecondsToday() that returns the number of seconds from the beginning of today.
+
+For instance, if now were 10:00 am, and there was no daylight savings shift, then:
+
+getSecondsToday() == 36000 // (3600 * 10)
+The function should work in any day. That is, it should not have a hard-coded value of “today”.
+ */
+
+function getSecondsToday() {
+  let now = new Date();
+  console.log(now.toString());
+
+  let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  console.log(today.toString());
+  return Math.round((now - today) / 1000);
+}
+console.log(getSecondsToday()); //== 36000. (3600 * 10)
