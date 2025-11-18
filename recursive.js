f71d1bb4d8fa623c1b489919dc692946e4888b53
vetacode <vetacode@gmail.com>
2025-11-18 10:13:11 +0700

    adding fastest power algorithm

--- recursive.js
+++ recursive.js
@@ -1,5 +1,5 @@
-//2 cara untuk menyelesaikan soal 2^3
-//1. Pake for loop
+//3 cara untuk menyelesaikan soal 2^3
+//1. Pake for loop. Time complexity O(n), space complexity O(1). Better than recursive
 function pow(x, n) {
   let result = 1;
   for (let i = 0; i < n; i++) {
@@ -9,7 +9,7 @@ function pow(x, n) {
 }
 console.log(pow(2, 3));
 
-//2. Pake Recursive. Efficiency O(n), slower than iterative loop, but its needed when a function uses different recursive subcalls, when branching is more intricate.
+//2. Pake Recursive. Efficiency time and space complexity both O(n), slower than iterative loop, but its needed when a function uses different recursive subcalls, when branching is more intricate.
 //Recursive give a shorter code, easier to understand and support.
 //Key is wisely balance between need of good code and optimization
 
@@ -26,3 +26,25 @@ function recursPowBest(x, n) {
   return n == 1 ? x : x * pow(x, n - 1);
 }
 console.log(recursPowBest(2, 3));
+
+//3. Pake Fastest power algorithm: Exponentiation by Squaring: both time & space complexity is log2(n) or O(log n)
+
+function fastPow(x, n) {
+  if (n === 0) {
+    return 1;
+  }
+  if (n % 2 === 0) {
+    return fastPow(x * x, n / 2);
+  }
+  return x * fastPow(x, n - 1);
+}
+console.log(fastPow(2, 3));
+
+/**SUMAMRY
+| Method                         | Time Complexity | Space Complexity | Notes                  |
+| ------------------------------ | --------------- | ---------------- | ---------------------- |
+| Iterative `pow`                | **O(n)**        | O(1)             | Slow for large n       |
+| Recursive simple               | **O(n)**        | O(n)             | Even worse stack usage |
+| **FastPow (By Squaring)**      | **O(log n)**    | O(log n)         | Fastest possible       |
+
+ */
