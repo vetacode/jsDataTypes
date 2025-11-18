de7ccde75480e9087c0501a3ee0f227b1424fa14
vetacode <vetacode@gmail.com>
2025-11-18 13:23:34 +0700

    task 2 using recursion factorial done

--- recursive.js
+++ recursive.js
@@ -212,3 +212,29 @@ function sumToProg(n) {
   return (n * (n + 1)) / 2;
 }
 console.log(sumToProg(100));
+
+/**TASK 2
+ * Calculate factorial
+importance: 4
+The factorial of a natural number is a number multiplied by "number minus one", then by "number minus two", and so on till 1. The factorial of n is denoted as n!
+
+We can write a definition of factorial like this:
+
+n! = n * (n - 1) * (n - 2) * ...*1
+Values of factorials for different n:
+
+1! = 1
+2! = 2 * 1 = 2
+3! = 3 * 2 * 1 = 6
+4! = 4 * 3 * 2 * 1 = 24
+5! = 5 * 4 * 3 * 2 * 1 = 120
+The task is to write a function factorial(n) that calculates n! using recursive calls.
+
+alert( factorial(5) ); // 120
+P.S. Hint: n! can be written as n * (n-1)! For instance: 3! = 3*2! = 3*2*1! = 6
+ */
+
+function factorial(n) {
+  return n === 1 ? 1 : n * factorial(n - 1);
+}
+console.log(factorial(5));
