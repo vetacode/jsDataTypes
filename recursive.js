ebd59a5377ee02a0b901a165ade10dfaf6a30d34
vetacode <vetacode@gmail.com>
2025-11-18 08:42:29 +0700

    adding recursive section

---
+++ recursive.js
@@ -0,0 +1,12 @@
+//2 cara untuk menyelesaikan soal 2^3
+//1. Pake for loop
+function pow(x, n) {
+  let result = 1;
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+console.log(pow(2, 3));
+
+//2. Pake Recursive
