396d9bf1b85e5b1ef30dc34608190a490f1b4b1d
vetacode <vetacode@gmail.com>
2025-11-18 12:30:57 +0700

    adding recursive structures and starting recursive tasks

--- recursive.js
+++ recursive.js
@@ -150,3 +150,44 @@ function formatUSD(num) {
 let totalUSD = totalSalaries(perusahaan);
 
 console.log(formatUSD(totalUSD)); //83000
+
+//RECURSIVE STRUCTURES
+//LINKED LIST is a fast insertion/deletion for listed objects
+//Syntax: list = {value: 'new item', next: list}
+let list = { value: 1 };
+list.next = { value: 2 };
+list.next.next = { value: 3 };
+list.next.next.next = { value: 4 };
+
+// prepend the new value to the list
+list = { value: 'new item', next: list };
+
+console.log(list);
+
+/**TASK 1
+ *Sum all numbers till the given one
+importance: 5
+Write a function sumTo(n) that calculates the sum of numbers 1 + 2 + ... + n.
+
+For instance:
+
+sumTo(1) = 1
+sumTo(2) = 2 + 1 = 3
+sumTo(3) = 3 + 2 + 1 = 6
+sumTo(4) = 4 + 3 + 2 + 1 = 10
+...
+sumTo(100) = 100 + 99 + ... + 2 + 1 = 5050
+Make 3 solution variants:
+
+Using a for loop.
+Using a recursion, cause sumTo(n) = n + sumTo(n-1) for n > 1.
+Using the arithmetic progression formula.
+An example of the result:
+
+function sumTo(n) { /... your code ... / }
+
+alert( sumTo(100) ); // 5050
+P.S. Which solution variant is the fastest? The slowest? Why?
+
+P.P.S. Can we use recursion to count sumTo(100000)?
+ */
