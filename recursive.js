18f80241d8d1c83c32e8896779ab83daee118918
vetacode <vetacode@gmail.com>
2025-11-18 09:29:04 +0700

    adding remarks for recursive function

--- recursive.js
+++ recursive.js
@@ -9,18 +9,20 @@ function pow(x, n) {
 }
 console.log(pow(2, 3));
 
-//2. Pake Recursive
+//2. Pake Recursive. Efficiency O(n), slower than iterative loop, but its needed when a function uses different recursive subcalls, when branching is more intricate.
+//Recursive give a shorter code, easier to understand and support.
+//Key is wisely balance between need of good code and optimization
+
 function recursPow(x, n) {
   if (n == 1) {
     return x;
   }
-  x *= pow(x, n - 1);
-  return x;
+  return x * pow(x, n - 1);
 }
 console.log(recursPow(2, 3));
 
 //Lean Code
 function recursPowBest(x, n) {
-  return n == 1 ? x : (x *= pow(x, n - 1));
+  return n == 1 ? x : x * pow(x, n - 1);
 }
 console.log(recursPowBest(2, 3));
