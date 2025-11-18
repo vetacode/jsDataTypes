0b38e0eee860d3481b0cda8a2e509e28acc566fe
vetacode <vetacode@gmail.com>
2025-11-18 14:04:17 +0700

    task 3 fibonacci done

--- recursive.js
+++ recursive.js
@@ -240,7 +240,7 @@ function factorial(n) {
   }
   return n <= 1 ? 1 : n * factorial(n - 1);
 }
-console.log(factorial(0));
+console.log(factorial(5));
 
 //alternative
 function factorial2(n) {
@@ -250,3 +250,34 @@ function factorial2(n) {
   return n ? n * factorial2(n - 1) : 1;
 }
 console.log(factorial2(0));
+
+/**TASK 3
+ * Fibonacci numbers
+importance: 5
+The sequence of Fibonacci numbers has the formula Fn = Fn-1 + Fn-2. In other words, the next number is a sum of the two preceding ones.
+
+First two numbers are 1, then 2(1+1), then 3(1+2), 5(2+3) and so on: 1, 1, 2, 3, 5, 8, 13, 21....
+
+Fibonacci numbers are related to the Golden ratio and many natural phenomena around us.
+
+Write a function fib(n) that returns the n-th Fibonacci number.
+
+An example of work:
+
+function fib(n) { / your code / }
+
+alert(fib(3)); // 2
+alert(fib(7)); // 13
+alert(fib(77)); // 5527939700884757
+P.S. The function should be fast. The call to fib(77) should take no more than a fraction of a second.
+ */
+
+function fib(n) {
+  if (n < 0 || !Number.isInteger(n)) {
+    throw new Error(`Error: put only non negative values`);
+  }
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+console.log(fib(3));
+console.log(fib(7));
+console.log(fib(10));
